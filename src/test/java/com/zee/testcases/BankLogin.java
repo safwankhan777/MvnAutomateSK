69fc42146d42694c022626c4755aae0891f1a338
safwan <safwan.khan619@gmail.com>
2023-08-19 00:02:03 +0530

    removed unwanted comments

--- src/test/java/com/zee/testcases/BankLogin.java
+++ src/test/java/com/zee/testcases/BankLogin.java
@@ -13,8 +13,7 @@ public class BankLogin extends TestBase {
         driver.findElement(By.xpath(OR.getProperty("BankManagerLoginBtn"))).click();
         log.debug("user clicked successfully for bank manager login button");
 
-        /*String base64ScreenshotContent = "error_base64.txt"; // Replace with actual Base64 content
-        Reporter.log("<a target=\"_blank\" href=\"file://Users/safwankhan/Documents/Selenium_Automation/MvnAutomateSK/src/test/resources/screenshot/error.png\">View Screenshot</a>");
+        /* Reporter.log("<a target=\"_blank\" href=\"file://Users/safwankhan/Documents/Selenium_Automation/MvnAutomateSK/src/test/resources/screenshot/error.png\">View Screenshot</a>");
         Reporter.log("<img src=\"data:image/png;base64," + base64ScreenshotContent + "\" alt=\"view screenshot\">");*/
 
         //Thread.sleep(3000);
